5f462d76903c765b994669afa326b973fab06ab5
sohrab <sohrab@sohrabhossain.com>
2025-11-07 10:19:01 +0000

    set up authentication and configuration for aws

---
+++ providers.tf
@@ -0,0 +1,6 @@
+
+provider "aws" {
+  shared_config_files      = ["/home/sohrab/.aws/config"]
+  shared_credentials_files = ["/home/sohrab/.aws/credentials"]
+  profile                  = "terraform_dockyard"
+}
