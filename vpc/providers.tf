09be049946e5962845d38b70d1c57c7387577ffd
sohrab <sohrab@sohrabhossain.com>
2025-11-08 11:12:55 +0000

    setup a multi teir vpc, add tfstate file to .gitignore

---
+++ vpc/providers.tf
@@ -0,0 +1,7 @@
+
+provider "aws" {
+  shared_config_files      = ["/home/sohrab/.aws/config"]
+  shared_credentials_files = ["/home/sohrab/.aws/credentials"]
+  profile                  = "terraform_dockyard"
+  region = "us-east-1"
+}
